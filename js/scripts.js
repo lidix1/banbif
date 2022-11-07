2bed47793d3403fc649c50e3c2f75872fc5b3c41
Lisbeth Guillen <lisguillen1@gmail.com>
2022-11-07 11:57:16 -0500

    Actualizacion

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+     $(document).ready(function() {
+              $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                responsiveClass: true,
+                dots: false,
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+
+                }
+              })
+            })
